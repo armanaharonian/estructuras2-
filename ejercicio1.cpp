0d192215d9453601fa0b3416f567fa2314e91e2b
armanaharonian <armanaharonian@gmail.com>
2026-04-23 21:25:19 -0300

    (feat): ejercicio1
    WARN: ejercicio 1 andando sin casos de pueba

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,22 +2,9 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "./tads/tablasHash/tablaHashAbierta/tablaHashAbierta.cpp"
 
 using namespace std;
-
-int main()
-{
-    int n;
-    cin >> n;
-
-    string s;
-    cin >> s;
-
-    /*cout << "n = " << n << endl;
-    cout << "s = " << s << endl;*/
-
-    return 0;
-}
 
 int ejercicio1(int n, string s) {
     TablaHash tabla(26);
@@ -93,3 +80,13 @@ int ejercicio1(int n, string s) {
 
     return llavesSolicitadas;
 }*/
+int main()
+{
+   int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    cout << ejercicio1(n,s) << endl; 
+}
+
